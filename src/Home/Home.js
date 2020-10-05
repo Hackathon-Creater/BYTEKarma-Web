5dd38d8b132ac7df18e2134f505852cb2c11e194
Manish Thete <manish.thete24@users.noreply.github.com>
2020-10-05 23:59:36 +0530

    Home page integration changes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,7 @@ import React, { state3, useState } from 'react';
 
 import { Bar, Doughnut, Line, Pie, HorizontalBar } from "react-chartjs-2";
 import { MDBContainer } from "mdbreact";
-import { Card, ProgressBar } from "react-bootstrap";
+import { Card, ProgressBar, DropdownButton,Dropdown} from "react-bootstrap";
 import axios from 'axios';
 import https from 'https';
 import fs from 'fs';
@@ -26,17 +26,90 @@ class Home extends React.Component {
             movie_avg: 0,
             ccbill_avg: 0,
             transPerDayLabelArray: [],
-            transPerDayDataArray: []
+            transPerDayDataArray: [],
+            lab : ""
 
 
         };
+
+   this.handleHomeCountryChange=this.handleHomeCountryChange.bind(this);
 
 
         // this.cif = React.createRef(); 
     }
 
+    handleHomeCountryChange(e) {
+        console.log(e.target.textContent);
+        axios({
+            method: 'post',
+            url: "http://18.221.237.209:5000/getCountryCategory",
+            headers: { 'Content-Type': 'application/json' },
+            data: {
+              "country":String (e.target.textContent)
+            }
+          }).then(function (response) {
+            
+            
+            const countryData =JSON.stringify(response);
+           
+            localStorage.setItem("ctry_label_1",response.data[0].SUMMARY_OF_TRANSACTION);
+            localStorage.setItem("ctry_label_2",response.data[1].SUMMARY_OF_TRANSACTION);
+            localStorage.setItem("ctry_label_3",response.data[2].SUMMARY_OF_TRANSACTION);
+
+            localStorage.setItem("ctry_label_1_data",response.data[0].AMOUNT_SPENT);
+            localStorage.setItem("ctry_label_2_data",response.data[1].AMOUNT_SPENT);
+            localStorage.setItem("ctry_label_3_data",response.data[2].AMOUNT_SPENT);
+
+
+            console.log(response.data[0].SUMMARY_OF_TRANSACTION);
+            console.log(response.data[0].AMOUNT_SPENT);
+           
+            window.location.reload(false);
+            
+          })
+        .catch((error) => {
+            console.log(error);
+        });
+      
+      }
+    
+
+      handleHomeRegionChange(e) {
+        console.log(e.target.textContent);
+        axios({
+            method: 'post',
+            url: "http://18.221.237.209:5000/getRegionCategory",
+            headers: { 'Content-Type': 'application/json' },
+            data: {
+              "region":String (e.target.textContent)
+            }
+          }).then(function (response) {
+      
+           
+            localStorage.setItem("reg_label_1",response.data[0].SUMMARY_OF_TRANSACTION);
+            localStorage.setItem("reg_label_2",response.data[1].SUMMARY_OF_TRANSACTION);
+            localStorage.setItem("reg_label_3",response.data[2].SUMMARY_OF_TRANSACTION);
+
+            localStorage.setItem("reg_label_1_data",response.data[0].AMOUNT_SPENT);
+            localStorage.setItem("reg_label_2_data",response.data[1].AMOUNT_SPENT);
+            localStorage.setItem("reg_label_3_data",response.data[2].AMOUNT_SPENT);
+
+
+            console.log(response.data[0].SUMMARY_OF_TRANSACTION);
+            console.log(response.data[0].AMOUNT_SPENT);
+           
+            window.location.reload(false);
+            
+          })
+        .catch((error) => {
+            console.log(error);
+        });
+      
+      }
+
 
     componentWillMount() {
+     
         var transPerDayLabel = [];
         var transPerDayData = [];
         var statusRes = "";
@@ -58,7 +131,7 @@ class Home extends React.Component {
                 localStorage.setItem("contacted_cust", data["cardDetails"][0].contactedCust);
                 localStorage.setItem("risky_cust", data["cardDetails"][0].riskyCust);
                 localStorage.setItem("gambling_avg", data["cardDetails"][0].gambAvg);
-                localStorage.setItem( "loan_Avg", data["cardDetails"][0].loanAvg );
+                localStorage.setItem("loan_Avg", data["cardDetails"][0].loanAvg);
                 // localStorage.setItem("amazon_avg",data["cardDetails"][0] );
                 localStorage.setItem("custAtObservation", data["cardDetails"][0].custAtObservation);
                 localStorage.setItem("custFailToImprove", data["cardDetails"][0].custFailToImprove);
@@ -78,20 +151,24 @@ class Home extends React.Component {
                 localStorage.setItem("shopPerc", data["spentCategoryPieChart"][0].shopPerc);
                 // console.log(data["transPerDay"].length);
 
-                for (let i = 0; i < data["transPerDay"].length; i++) {
+                //horizontal bar Globally 
+                localStorage.setItem("globalGamblingSpend", data["spendCategoryGlobally"][0].amountSpent);
+                localStorage.setItem("globalLoanReturn", data["spendCategoryGlobally"][1].amountSpent);
+                localStorage.setItem("globalCreditCardDetail", data["spendCategoryGlobally"][2].amountSpent);
 
-                    transPerDayLabel.push(data["transPerDay"][i].dateOfSpend);
-                    transPerDayData.push(data["transPerDay"][i].amountSpent);
-                }
+                localStorage.setItem("Jan_Data", data["transPerDay"][0].avgAmountSpent);
+                localStorage.setItem("Feb_Data", data["transPerDay"][1].avgAmountSpent);
+                localStorage.setItem("mar_Data", data["transPerDay"][2].avgAmountSpent);
+                localStorage.setItem("apr_Data", data["transPerDay"][3].avgAmountSpent);
+                localStorage.setItem("may_Data", data["transPerDay"][4].avgAmountSpent);
+                localStorage.setItem("june_Data", data["transPerDay"][5].avgAmountSpent);
+                localStorage.setItem("july_Data", data["transPerDay"][6].avgAmountSpent);
+                localStorage.setItem("aug_Data", data["transPerDay"][7].avgAmountSpent);
+                localStorage.setItem("sep_Data", data["transPerDay"][8].avgAmountSpent);
+                localStorage.setItem("oct_Data", data["transPerDay"][9].avgAmountSpent);
+                localStorage.setItem("nov_Data", data["transPerDay"][10].avgAmountSpent);
+                localStorage.setItem("dec_Data", data["transPerDay"][11].avgAmountSpent);
 
-                // localStorage.setItem("transPerDayLabel",transPerDayLabel);
-                // localStorage.setItem("transPerDayData",transPerDayData);
-                // this.setState({
-                //     transPerDayLabelArray: transPerDayLabel,
-                //     transPerDayDataArray:transPerDayData
-                // })
-
-                // console.log(this.state.transPerDayLabel===undefined?0:this.state.transPerDayLabel);
 
 
             })
@@ -194,11 +271,10 @@ class Home extends React.Component {
 
 
         boundryAreadata: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug'],
-            // labels:localStorage.getItem("transPerDayLabel"),
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug','Sep','Oct','Nov','Dec'],
             datasets: [
                 {
-                    label: 'Visit by Month of Year',
+                    label: 'Month wise data',
                     fill: true,
                     lineTension: 0.1,
                     backgroundColor: 'skyblue',
@@ -216,8 +292,10 @@ class Home extends React.Component {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                    // data:localStorage.getItem("transPerDayData")
+                    data: [localStorage.getItem("Jan_Data"),localStorage.getItem("Feb_Data"),localStorage.getItem("mar_Data"),localStorage.getItem("apr_Data"),localStorage.getItem("may_Data"),localStorage.getItem("june_Data"),localStorage.getItem("july_Data"),localStorage.getItem("aug_Data"),localStorage.getItem("sep_Data"),localStorage.getItem("oct_Data"),localStorage.getItem("nov_Data"),localStorage.getItem("dec_Data")]
+
+         
+                   
                 }
             ]
         }
@@ -258,18 +336,52 @@ class Home extends React.Component {
             }]
         }
     }
-    horizontalBar = {
+    horizontalBar1 = {
         data: {
-            labels: ['US', 'UK', 'China'],
+            labels: ['Gambling', 'Loan return', 'Credit Card Bill'],
             datasets: [
                 {
                     label: 'Sepending Customers',
-                    backgroundColor: 'skyblue',
-                    borderColor: 'rgba(255,99,132,1)',
+                    backgroundColor: 'turquoise',
+                    borderColor: 'darkcyan',
                     borderWidth: 1,
-                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                    hoverBorderColor: 'rgba(255,99,132,1)',
-                    data: [65, 59, 80]
+                    hoverBackgroundColor: 'darkcyan',
+                    hoverBorderColor: 'turquoise',
+                    data: [localStorage.getItem("globalGamblingSpend"), localStorage.getItem("globalLoanReturn"), localStorage.getItem("globalCreditCardDetail")]
+                }
+            ]
+        }
+    }
+
+    horizontalBar2 = {
+        data: {
+            labels: [localStorage.getItem("ctry_label_1"), localStorage.getItem("ctry_label_2"), localStorage.getItem("ctry_label_3")],
+            datasets: [
+                {
+                    label: 'Sepending Customers',
+                    backgroundColor: 'lightGreen',
+                    borderColor: 'limegreen',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'limegreen',
+                    hoverBorderColor: 'lightGreen',
+                    data: [localStorage.getItem("ctry_label_1_data"), localStorage.getItem("ctry_label_2_data"), localStorage.getItem("ctry_label_3_data")]
+                }
+            ]
+        }
+    }
+
+    horizontalBar3 = {
+        data: {
+            labels: [localStorage.getItem("reg_label_1"), localStorage.getItem("reg_label_2"), localStorage.getItem("reg_label_3")],
+            datasets: [
+                {
+                    label: 'Sepending Customers',
+                    backgroundColor: 'orange',
+                    borderColor: 'orangered',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'orangered',
+                    hoverBorderColor: 'orange',
+                    data: [localStorage.getItem("reg_label_1_data"), localStorage.getItem("reg_label_2_data"), localStorage.getItem("reg_label_3_data")]
                 }
             ]
         }
@@ -314,28 +426,28 @@ class Home extends React.Component {
                     backgroundColor: '#c3edea',
                     borderColor: 'darkgreen',
                     borderWidth: 1,
-                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBackgroundColor: 'lightseagreen',
                     hoverBorderColor: 'rgba(255,99,132,1)',
-                    data: [localStorage.getItem("risky_cust"),localStorage.getItem("total_cust")-localStorage.getItem("risky_cust")]
+                    data: [localStorage.getItem("risky_cust"), localStorage.getItem("total_cust") - localStorage.getItem("risky_cust")]
                 }
             ]
         }
     }
 
- 
+
     barChartDaily = {
         data: {
             labels: ['Credit Card', 'Gambling', 'Food', 'Loan', 'Movie', 'Shoping'],
             datasets: [
                 {
-                    label: 'Daily Status',
+                    label: 'Average Spending',
                     backgroundColor: 'rgba(255,99,132,0.2)',
                     borderColor: 'rgba(255,99,132,1)',
                     borderWidth: 1,
                     stack: 1,
                     hoverBackgroundColor: 'rgba(255,99,132,0.4)',
                     hoverBorderColor: 'rgba(255,99,132,1)',
-                    data: [localStorage.getItem("ccbill_avg"),localStorage.getItem("gambling_avg"),localStorage.getItem("food_avg"),localStorage.getItem("loan_Avg"),localStorage.getItem("movie_avg"),localStorage.getItem("shopping_avg")]
+                    data: [localStorage.getItem("ccbill_avg"), localStorage.getItem("gambling_avg"), localStorage.getItem("food_avg"), localStorage.getItem("loan_Avg"), localStorage.getItem("movie_avg"), localStorage.getItem("shopping_avg")]
                 },
             ]
         }
@@ -497,7 +609,7 @@ class Home extends React.Component {
                     <div class="row" style={{ marginTop: '2%' }}>
                         <div class="col-sm-6">
                             <h4>
-                                Customer Visited by Month of Year
+                                User Spending Pattern of Last Year
                         </h4>
                             <Line data={this.boundryArea.boundryAreadata} />
                         </div>
@@ -514,9 +626,9 @@ class Home extends React.Component {
                         </div>
                     </div>
                     <br></br>
-                    <div class="row" style={{ marginTop: '2%' }}>
-                        <div class="col-sm-4">
-                            <h4>Defaulted vs. Non-Defaulted users
+                    <div class="row" style={{ marginTop: '2%', width: '95%' }}>
+                        <div class="col-sm-4" >
+                            <h4>Projected Defaulted vs. Non-Projected Defaulted Users
 
 
 </h4>
@@ -531,7 +643,10 @@ class Home extends React.Component {
 
                             />
                         </div>
-                        <div class="col-sm-8">
+                        {/* <div class="col-sm-2">
+
+                        </div> */}
+                        <div class="col-sm-8"  >
                             <h4>Average Spending of Customers
 
 
@@ -549,22 +664,43 @@ class Home extends React.Component {
                         </div>
                     </div>
                     <br></br>
-                    <div class="row">
-                        <div class="col-sm-4">
+                    <div class="row" style={{ marginTop: '2%' }}>
+                        <div class="col-sm-4" style={{ marginLeft: '-1%' }}>
                             <h4>Top 3 Spending categories of Customers by Globally
 </h4>
-                            <HorizontalBar data={this.horizontalBar.data} />
+<div style={{ marginTop: '5%' }}>
+
+
+                            <HorizontalBar  data={this.horizontalBar1.data} />
+                            </div>
                         </div>
 
                         <div class="col-sm-4">
                             <h4>Top 3 Spending categories of Customers by Country
 </h4>
-                            <HorizontalBar data={this.horizontalBar.data} />
+                            <div>
+                                <DropdownButton id="dropdown-basic-button" title="Select Country" size="lg">
+                                    <Dropdown.Item onClick={(e) => this.handleHomeCountryChange(e)}>England</Dropdown.Item>
+                                    <Dropdown.Item onClick={(e) => this.handleHomeCountryChange(e)}>Australia</Dropdown.Item>
+                                    <Dropdown.Item onClick={(e) => this.handleHomeCountryChange(e)}>United States of America</Dropdown.Item>
+                                    <Dropdown.Item onClick={(e) => this.handleHomeCountryChange(e)}>India</Dropdown.Item>
+                                </DropdownButton>
+                            </div>
+                            <HorizontalBar data={this.horizontalBar2.data} />
                         </div>
                         <div class="col-sm-4">
                             <h4>Top 3 Spending categories of Customers by Region
+
+                            <div>
+                                <DropdownButton id="dropdown-basic-button" title="Select Region" style={{marginTop:'1%'}} size="lg">
+                                    <Dropdown.Item onClick={(e) => this.handleHomeRegionChange(e)}>Americas</Dropdown.Item>
+                                    <Dropdown.Item onClick={(e) => this.handleHomeRegionChange(e)}>APAC</Dropdown.Item>
+                                    <Dropdown.Item onClick={(e) => this.handleHomeRegionChange(e)}>EUR</Dropdown.Item>
+                                  
+                                </DropdownButton>
+                            </div>
 </h4>
-                            <HorizontalBar data={this.horizontalBar.data} />
+                            <HorizontalBar data={this.horizontalBar3.data} />
                         </div>
                     </div>
                     <br></br>
